c61f15425babac21932ae6c41cc73330dbf26cab
siloudetanke <siloudetanke@users.noreply.github.com>
2021-10-06 13:40:22 +0200

    Added Login page

---
+++ Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using CamperPlanner.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(CamperPlanner.Areas.Identity.IdentityHostingStartup))]
+namespace CamperPlanner.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
