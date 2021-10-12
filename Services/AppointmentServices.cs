21d851f8a4c5c8386a3257218d921fc85741ce09
simonypkemeule <0323343@student.rocvantwente.nl>
2021-10-12 13:30:33 +0200

    ViewModel toegevoegd

---
+++ Services/AppointmentServices.cs
@@ -0,0 +1,21 @@
+﻿using CamperPlanner.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CamperPlanner.Services
+{
+    public class AppointmentServices : IAppointmentService
+    {
+        public List<DoctorViewModel> GetDoctorList()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<PatientViewModel> GetPatientList()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
